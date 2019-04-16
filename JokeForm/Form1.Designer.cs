8df7d35936ef71eaa1c62fb818a3cca897c59b72
Zachary Bitseff <zachbitseff@gmail.com>
2019-04-16 01:16:41 -0700

    Created WinForms Project with "Get Joke" button, and dropdown list for categories.

---
+++ JokeForm/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace JokeForm
+{
+    partial class JokeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoriesComboBox = new System.Windows.Forms.ComboBox();
+            this.categoriesLabel = new System.Windows.Forms.Label();
+            this.getJokeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // categoriesComboBox
+            // 
+            this.categoriesComboBox.FormattingEnabled = true;
+            this.categoriesComboBox.Location = new System.Drawing.Point(15, 30);
+            this.categoriesComboBox.Name = "categoriesComboBox";
+            this.categoriesComboBox.Size = new System.Drawing.Size(121, 21);
+            this.categoriesComboBox.TabIndex = 0;
+            // 
+            // categoriesLabel
+            // 
+            this.categoriesLabel.AutoSize = true;
+            this.categoriesLabel.Location = new System.Drawing.Point(12, 9);
+            this.categoriesLabel.Name = "categoriesLabel";
+            this.categoriesLabel.Size = new System.Drawing.Size(60, 13);
+            this.categoriesLabel.TabIndex = 1;
+            this.categoriesLabel.Text = "Categories:";
+            // 
+            // getJokeBtn
+            // 
+            this.getJokeBtn.Location = new System.Drawing.Point(148, 12);
+            this.getJokeBtn.Name = "getJokeBtn";
+            this.getJokeBtn.Size = new System.Drawing.Size(115, 42);
+            this.getJokeBtn.TabIndex = 2;
+            this.getJokeBtn.Text = "Get Joke!";
+            this.getJokeBtn.UseVisualStyleBackColor = true;
+            this.getJokeBtn.Click += new System.EventHandler(this.GetJokeBtn_ClickAsync);
+            // 
+            // JokeForm
+            // 
+            this.AcceptButton = this.getJokeBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 64);
+            this.Controls.Add(this.getJokeBtn);
+            this.Controls.Add(this.categoriesLabel);
+            this.Controls.Add(this.categoriesComboBox);
+            this.Name = "JokeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Joke Form";
+            this.Load += new System.EventHandler(this.JokeForm_LoadAsync);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox categoriesComboBox;
+        private System.Windows.Forms.Label categoriesLabel;
+        private System.Windows.Forms.Button getJokeBtn;
+    }
+}
+
---
+++ JokeForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChuckNorrisAPI;
+
+namespace JokeForm
+{
+    public partial class JokeForm : Form
+    {
+        public JokeForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetJokeBtn_ClickAsync(object sender, EventArgs e)
+        {
+            Joke j = await ChuckNorrisClient.GetRandomJoke();
+
+            /*
+            string sel = categoriesComboBox.SelectedItem.ToString();
+            //MessageBox.Show(sel, "test");
+            string cat = j.Categories.ToString();
+
+            if ( !(cat == sel) ) {
+
+                while (!(cat == sel))
+                {
+                    j = await ChuckNorrisClient.GetRandomJoke();
+                    cat = j.Categories.ToString();
+                }
+
+            }
+            */
+
+            String text = j.Id + " : " + j.JokeText;
+            MessageBox.Show(text, "Joke Display");
+
+        }
+
+        private async void JokeForm_LoadAsync(object sender, EventArgs e)
+        {
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            categoriesComboBox.DataSource = categories;
+        }
+    }
+}
